07175e7b76295bf8c3d450660893d0f4e2a532b1
gxx <xx.gui@ctrip.com>
2020-04-16 16:38:08 +0800

    mongod

--- nest-example/src/app.module.ts
+++ nest-example/src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { CatsController } from './catsController';
 import { AppService } from './app.service';
 import { CatsService } from './catsService';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FetchModule } from './fetch';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [FetchModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-blog', {
+      useNewUrlParser: true,
+    }),
+    FetchModule,
+    BlogModule,
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
