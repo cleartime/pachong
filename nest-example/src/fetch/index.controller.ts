34dc1fc5fcb6293d62da8ffc2c138c139f7a4474
gxx <xx.gui@ctrip.com>
2020-04-17 17:35:11 +0800

    debug

--- nest-example/src/fetch/index.controller.ts
+++ nest-example/src/fetch/index.controller.ts
@@ -4,7 +4,13 @@ import { IndexService } from './index.service';
 @Controller('fetch')
 export class IndexController {
   constructor(private indexService: IndexService) {}
-
+  @Post()
+  findAll2() {
+    return {
+      sfddsf:2,
+      sdfds:3
+    };
+  }
   @Get()
   findAll() {
     this.indexService.findAll();
