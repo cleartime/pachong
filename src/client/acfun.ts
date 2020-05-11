1232e392d1007b45d8900c7d34863a2275635741
gxx <xx.gui@trip.com>
2020-05-11 19:35:19 +0800

    resposn

--- src/client/acfun.ts
+++ src/client/acfun.ts
@@ -1,18 +1,19 @@
 const puppeteer = require('puppeteer');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const config = {
   account: '17095739373',
   password: 'Gxx562606139',
   url: 'https://www.acfun.cn/login/',
   article_url: 'https://member.acfun.cn/post-article',
-  capmoneycourses_Api: 'https://capuk.org/ajax_search/capmoneycourses'
-}
+  channel_url: 'https://member.acfun.cn/common/api/getChannelList',
+  capmoneycourses_Api: 'https://capuk.org/ajax_search/capmoneycourses',
+};
 
-export const acfunlogin = async function(option:any = {}) {
-  const { title = '', des = '', content = '', id = ''} = option;
-  if(!id) return
-  console.log('打开网站'); 
+export const acfunlogin = async function(option: any = {}) {
+  const { title = '', des = '', content = '', id = '' } = option;
+  // if(!id) return
+  console.log('打开网站');
   const browser = await puppeteer.launch({
     ignoreHTTPSErrors: true,
     headless: false,
@@ -22,6 +23,21 @@ export const acfunlogin = async function(option:any = {}) {
   const page = await browser.newPage();
   await page.setJavaScriptEnabled(true);
   await page.goto(config.url);
+  await page.on('response', async response => {
+    if (response.url() !== config.article_url) return;
+    const res = await response.json()
+    const re2 = await res
+    console.log(re2);
+  });
+  // function getResponseBody(resolve, reject){
+  //   page.on('response', async function(response){
+  //        if(response.url().includes(config.article_url)){
+  //             resolve(await response.text())
+  //        }
+  //   })
+  // }
+  // const res = await new Promise(getResponseBody)
+  // console.log(res)
   const accountswitchx = await page.$x('//*[@id="login-account-switch"]');
   await accountswitchx[0].click();
   const account = await page.$('#ipt-account-login');
@@ -36,7 +52,9 @@ export const acfunlogin = async function(option:any = {}) {
   await page.waitFor(5000);
   await page.goto(config.article_url);
   console.log('输入标题');
-  const tit = await page.$x('/html/body/div[1]/div[2]/div[2]/div/div/div/form/div[1]/div[2]/div[2]/div/div[1]/input');
+  const tit = await page.$x(
+    '/html/body/div[1]/div[2]/div[2]/div/div/div/form/div[1]/div[2]/div[2]/div/div[1]/input',
+  );
   await tit[0].focus();
   await page.keyboard.type(title);
   console.log('选择分区');
@@ -73,19 +91,19 @@ export const acfunlogin = async function(option:any = {}) {
   );
   await tag4[0].click();
   console.log('输入简介');
-  
-  const description  = await page.$x(
+
+  const description = await page.$x(
     '/html/body/div[1]/div[2]/div[2]/div/div/div/form/div[6]/div[2]/div/div/div/textarea',
   );
   await description[0].focus();
-  await page.keyboard.type(des.slice(0,50));
-  console.log('输入正文')
+  await page.keyboard.type(des.slice(0, 50));
+  console.log('输入正文');
   const editor = await page.$('.ql-editor.ql-blank');
   await editor.focus();
-  await page.keyboard.down('Control')
-  await page.keyboard.down('v')
-  await page.keyboard.up('Control')
-  await page.keyboard.up('v')
+  await page.keyboard.down('Control');
+  await page.keyboard.down('v');
+  await page.keyboard.up('Control');
+  await page.keyboard.up('v');
   // await page.keyboard.type(content);
   // const link = await page.evaluate((editor, content) => {
   //   editor.innerHtml = content
--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -2,8 +2,8 @@ const schedule = require('node-schedule');
 import { acfunlogin, geekLogin, getContent } from '../client'
 import { from } from 'rxjs';
 export const scheduleCronstyle = async () => {
-  const res = await getContent()
-  acfunlogin(res);
+  // const res = await getContent()
+  acfunlogin();
   //每分钟的第30秒定时执行一次:
   // schedule.scheduleJob('00 07 09 * * *',()=>{
   //   geekLogin();
