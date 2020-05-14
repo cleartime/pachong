112e0f11d2a1f588d307eb6bf6ea14aa0efe6b0e
gxx <xx.gui@trip.com>
2020-05-14 19:34:03 +0800

    123124

--- compress.js
+++
@@ -1,9 +0,0 @@
-const compressing = require('compressing');
-
-compressing.zip.compressDir('./dist', './dist.zip')
-.then(() => {
-    console.log('success');
-})
-.catch(err => {
-    console.error(err);
-});
